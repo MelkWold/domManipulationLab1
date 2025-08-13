1f195a5e4447110edfef3f55718d5690d222bbbf
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-08-13 15:15:12 -0400

    Lab Two Parts 1 to 3 done

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,21 @@
 
 // Menu data structure
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+    ]},
     ];
 
 // =============================== Part 1: Getting Started ===========================================
@@ -39,7 +50,7 @@ console.log(topMenuEl)
 topMenuEl.style.height = "100%";
 
 // Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
-topMenuEl.style.backgroundColor = "var(--top-menu-bg";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 // Add a class of flex-around to topMenuEl.
 topMenuEl.classList.add("flex-around");
 
@@ -53,30 +64,62 @@ topMenuEl.classList.add("flex-around");
 
 // Append the new element to the topMenuEl element.
 
-for ( let i = 0; i < menuLinks.length; i++) {
-    let a = document.createElement("a");
-    a.classList.add("href");
-    a.href = menuLinks[i].href;
-    topMenuEl.attendChild(a);
-    console.log(a);
-    console.log(menuLinks)
-};
+// for (let i = 0; i < menuLinks.length; i++) {
+//     let a = document.createElement("a");
+//     a.classList.add("href");
+//     a.href = menuLinks[i].href;
+//     topMenuEl.attendChild(a);
+//     console.log(a);
+//     console.log(menuLinks)
+// };
 
 // Question:
 // Why is it that I didn't see the menu items on top menu eventhough I have added newEL. 
 
 // Dylans's approach
-// for (let link of menuLinks) {
-//     let menuButton = document.createElement("a");
-//     menuButton.setAttribute("href", link.href);
-//     menuButton.textContent = link.text;
-//     topMenuEl.appendChild(menuButton);
-// }
-
-
-
+for (let link of menuLinks) {
+    let menuButton = document.createElement("a");
+    menuButton.setAttribute("href", link.href);
+    menuButton.textContent = link.text;
+    topMenuEl.appendChild(menuButton);
+}
 
 // ====================== Part 4: Adding Interactivity ========================================
 // With the basic structure of the page now generated purely with JavaScript, we have demonstrated the ability to manipulate the DOM in several fundamental ways.
 // In order to continue with this project, we must first explore how to add user interaction to DOM elements, which will be covered in a future lesson. For now, save your work so that you can return to it for Part Two of this lab activity.
 // Remember to submit the link to this part of the project to Canvas using the submission instructions at the beginning of this document.
+
+// ================================ DOM Manipulation (Part Two) ======================================
+
+// ================ Part 1: Getting Started =======================
+// Take a few moments to explore your code and refamiliarize yourself with it. Now that you have a deeper understanding of DOM manipulation concepts, if there is anything you would like to fix or change, now is the time to do so.
+
+//Done!
+
+
+// ================ Part 2: Adding Additional HTML and CSS =======================
+// Done!
+
+// ================ Part 3: Creating the Submenu =======================
+// 1. Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
+let subMenuEl = document.getElementById("sub-menu");
+console.log(subMenuEl)
+
+// 2. Set the height subMenuEl element to be "100%"
+subMenuEl.style.height = "100%";
+// 3. Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+
+// 4. Add the class of flex-around to the subMenuEl element.
+subMenuEl.classList.add("flex-around");
+
+// Now, change the position of the submenu to temporarily hide it. Later, we will make the submenu appear dynamically based on user interaction:
+
+// Set the CSS position property of subMenuEl to the value of absolute.
+subMenuEl.style.position = "absolute";
+// Set the CSS top property of subMenuEl to the value of 0.
+subMenuEl.style.top = "0";
+
+// ================ Part 4: Adding Menu Interaction =======================
+// Update the menuLinks array
+    // Done!
